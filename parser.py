57357c013b4f9f5716a436cd33e0090c37a771e6
NKTKLN <66900108+NKTKLN@users.noreply.github.com>
2020-12-01 00:55:14 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,19 +1,26 @@
-# -*- coding: utf-8 -*-
-
 import requests, time
-
-parsertokens = [''] # x5
-url = '////'
+# # #
+parsertokens = ['0123456789:ABC10DEFGhIJKlmnO1P1QrS-tuvw1XYZaB1',
+                '0123456789:ABC10DEFGhIJKlmnO1P1QrS-tuvw1XYZaB1',
+                '0123456789:ABC10DEFGhIJKlmnO1P1QrS-tuvw1XYZaB1',
+                '0123456789:ABC10DEFGhIJKlmnO1P1QrS-tuvw1XYZaB1',
+                '0123456789:ABC10DEFGhIJKlmnO1P1QrS-tuvw1XYZaB1']
+token = '0123456789:ABC10DEFGhIJKlmnO1P1QrS-tuvw1XYZaB1'
+chat_id = '@chanal_name'
+chanals = ['chanal_name', 'chanal_name', 'chanal_name', '...']
+# #
+url = ''
 num = 0
 urllast = []
-
+# 
 while True:
-    chanals = [' ']
     for chanal in chanals:
         parsertoken = parsertokens[num]
         respons = requests.get('https://api.vk.com/method/wall.get', params={"access_token": parsertoken, "v": 5.124, "domain": chanal})
         try:
-            if chanal in [' ']:
+            type1 = respons.json()['response']['items'][00]['attachments'][0]['type']
+            type2 = respons.json()['response']['items'][1]['attachments'][0]['type'] 
+            if respons.json()['response']['items'][00]['attachments'][0][type1]['date'] > respons.json()['response']['items'][1]['attachments'][0][type2]['date']:
                 if respons.json()['response']['items'][00]['marked_as_ads'] == 0:
                     if len(respons.json()['response']['items'][00]['text']) < 50:
                         if 'copyright' not in respons.json()['response']['items'][00]:
@@ -22,7 +29,7 @@ while True:
                                     url = respons.json()['response']['items'][00]['attachments'][0]['photo']['sizes'][7]['url']
                                 except:
                                     url = respons.json()['response']['items'][00]['attachments'][0]['photo']['sizes'][6]['url']
-            elif chanal in [' ']:
+            else:
                 if respons.json()['response']['items'][1]['marked_as_ads'] == 0:
                     if len(respons.json()['response']['items'][1]['text']) < 50:
                         if 'copyright' not in respons.json()['response']['items'][00]:
@@ -31,9 +38,9 @@ while True:
                                     url = respons.json()['response']['items'][1]['attachments'][0]['photo']['sizes'][7]['url']
                                 except:
                                     url = respons.json()['response']['items'][1]['attachments'][0]['photo']['sizes'][6]['url']
-            if url.split("/")[3] + url.split("/")[4] not in urllast and url != '////':
-                url2 = 'https://api.telegram.org/bot<TOKEN>/sendPhoto'
-                requests.post(url=url2, data={'chat_id': ' ', 'photo': url})
+            if url.split("/")[3] + url.split("/")[4] not in urllast and url != '':
+                url2 = f'https://api.telegram.org/bot{token}/sendPhoto'
+                requests.post(url=url2, data={'chat_id': chat_id, 'photo': url})
                 urllast.append(url.split("/")[3] + url.split("/")[4])
             num += 1
             if num == len(parsertokens) - 1:
