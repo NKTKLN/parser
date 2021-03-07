2bc727020ea62a424771c02d3ad8445dce461dac
NKTKLN <66900108+NKTKLN@users.noreply.github.com>
2021-03-07 16:41:43 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,4 +1,7 @@
-import requests, time
+import requests
+import time
+import telebot
+from telebot.types import InputMediaPhoto
 # # #
 parsertokens = ['01234567890a1b1c121d3f1gh4151617181920i212j2k2l3m2425n26o2p7q2r8s2t930',
                 '01234567890a1b1c121d3f1gh4151617181920i212j2k2l3m2425n26o2p7q2r8s2t930',
@@ -8,11 +11,13 @@ parsertokens = ['01234567890a1b1c121d3f1gh4151617181920i212j2k2l3m2425n26o2p7q2r
 token = '0123456789:ABC10DEFGhIJKlmnO1P1QrS-tuvw1XYZaB1'
 chat_id = '@chanal_name'
 chanals = ['chanal_name', 'chanal_name', 'chanal_name', '...']
-# #
-url = ''
+# # #
+bot = telebot.TeleBot(token)
+# # 
 num = 0
 urllast = []
-# 
+timer = 0 
+#
 while True:
     for chanal in chanals:
         parsertoken = parsertokens[num]
@@ -24,27 +29,32 @@ while True:
                 if respons.json()['response']['items'][00]['marked_as_ads'] == 0:
                     if len(respons.json()['response']['items'][00]['text']) < 50:
                         if 'copyright' not in respons.json()['response']['items'][00]:
-                            if len(respons.json()['response']['items'][00]['attachments']) == 1:
-                                try:
-                                    url = respons.json()['response']['items'][00]['attachments'][0]['photo']['sizes'][7]['url']
-                                except:
-                                    url = respons.json()['response']['items'][00]['attachments'][0]['photo']['sizes'][6]['url']
+                            urs = respons.json()['response']['items'][00]['attachments']
+                                
             else:
                 if respons.json()['response']['items'][1]['marked_as_ads'] == 0:
                     if len(respons.json()['response']['items'][1]['text']) < 50:
-                        if 'copyright' not in respons.json()['response']['items'][00]:
-                            if len(respons.json()['response']['items'][1]['attachments']) == 1:
-                                try:
-                                    url = respons.json()['response']['items'][1]['attachments'][0]['photo']['sizes'][7]['url']
-                                except:
-                                    url = respons.json()['response']['items'][1]['attachments'][0]['photo']['sizes'][6]['url']
-            if url.split("/")[3] + url.split("/")[4] not in urllast and url != '':
-                url2 = f'https://api.telegram.org/bot{token}/sendPhoto'
-                requests.post(url=url2, data={'chat_id': chat_id, 'photo': url})
-                urllast.append(url.split("/")[3] + url.split("/")[4])
+                        if 'copyright' not in respons.json()['response']['items'][1]:
+                            urs = respons.json()['response']['items'][1]['attachments']
+
+            if chanal == 'morsik':
+                cap = '#comics'
+            else:
+                cap = '#meme'
+            try:
+                url = [InputMediaPhoto(urs[0]['photo']['sizes'][7]['url'], caption=cap)] + [InputMediaPhoto(urs[i + 1]['photo']['sizes'][6]['url']) for i in range(len(urs) - 1)]
+            except:
+                url = [InputMediaPhoto(urs[0]['photo']['sizes'][6]['url'], caption=cap)] + [InputMediaPhoto(urs[i + 1]['photo']['sizes'][6]['url']) for i in range(len(urs) - 1)]
+                                        
+            if urs[0]['photo']['sizes'][6]['url'] not in urllast:
+                bot.send_media_group(chat_id, url)
+                urllast.append(urs[0]['photo']['sizes'][6]['url'])
             num += 1
             if num == len(parsertokens) - 1:
                 num = 0
-            time.sleep(30)
+                time.sleep(65)
         except:
             pass
+    timer += 1
+    if timer == "100000000":
+        urllast.clear()
